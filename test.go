3e05d49727bb086eaad4038fb5c46df36dfbcf88
ASNordroid <n3tstrang3r@yandex.ru>
2018-08-28 15:32:30 +0300

    Refactor json parser

--- test.go
+++ test.go
@@ -1,38 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 	data, err := ioutil.ReadFile("C:\\Users\\Ant\\Desktop\\Sessions - 10-08-2018 06-05-44.json")
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	var decodedData []map[string]interface{}
-	if err := json.Unmarshal(data, &decodedData); err != nil {
-		panic(err)
-	}
+	err = json.Unmarshal(data, &decodedData)
+	check(err)
 
-	f, err := os.Create("sessionsUrls.txt")
-	if err != nil {
-		panic(err)
-	}
+	f, err := os.Create("C:\\Users\\Ant\\Desktop\\sessionsUrls.txt")
+	check(err)
 	defer f.Close()
-	w := bufio.NewWriter(f)
-	defer w.Flush()
 
 	for _, v := range decodedData {
-		for _, v2 := range v["windows"].(map[string]interface{}) {
-			for _, v3 := range v2.(map[string]interface{}) {
-				var t = v3.(map[string]interface{})
-				fmt.Fprint(w, t["url"], "\n")
-				fmt.Fprint(w, t["title"], "\n\n")
+		for _, v1 := range v["windows"].(map[string]interface{}) {
+			for _, v2 := range v1.(map[string]interface{}) {
+				var t = v2.(map[string]interface{})
+				f.WriteString(t["url"].(string) + "\n" + t["title"].(string) + "\n\n")
 			}
 		}
 	}
